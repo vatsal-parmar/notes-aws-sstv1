7a6a14dbd406208efe54d9c83e22db1b8912eb8b
vatsal-parmar <vatsalparmar33@gmail.com>
2022-06-08 07:33:29 +0530

    Securing the API

--- backend/functions/list.js
+++ backend/functions/list.js
@@ -1,7 +1,7 @@
 import handler from '../util/handler';
 import dynamoDb from '../util/dynamodb';
 
-export const main = handler(async () => {
+export const main = handler(async (event) => {
   const params = {
     TableName: process.env.TABLE_NAME,
 
@@ -13,7 +13,7 @@ export const main = handler(async () => {
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be the id of the author
     ExpressionAttributeValues: {
-      ':userId': '123',
+      ':userId': event.requestContext.authorizer.iam.cognitoIdentity.identityId,
     },
   };
 
