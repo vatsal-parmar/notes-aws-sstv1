32a2e4293cfc7bb2e4345165c4e1f7f859e33154
vatsal-parmar <vatsalparmar33@gmail.com>
2022-06-17 05:52:12 +0530

    Building our React app

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,12 +2,50 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
+import NewNote from './containers/NewNote';
+import Notes from './containers/Notes';
+import AuthenticatedRoute from './components/AuthenticatedRoute';
+import UnauthenticatedRoute from './components/UnauthenticatedRoute';
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />;
+      <Route path="*" element={<NotFound />} />
+      <Route
+        path="/login"
+        element={
+          <UnauthenticatedRoute>
+            <Login />
+          </UnauthenticatedRoute>
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <UnauthenticatedRoute>
+            <Signup />
+          </UnauthenticatedRoute>
+        }
+      />
+      <Route
+        path="/notes/new"
+        element={
+          <AuthenticatedRoute>
+            <NewNote />
+          </AuthenticatedRoute>
+        }
+      />
+      <Route
+        path="/notes/:id"
+        element={
+          <AuthenticatedRoute>
+            <Notes />
+          </AuthenticatedRoute>
+        }
+      />
     </Routes>
   );
 }
